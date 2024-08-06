692ee812b4eb7e608d014088f206c98e7da17ed6
ShobyG <shobysekar@gmail.com>
2024-08-05 21:28:20 -0700

    add item is now responsive

--- src/Item.js
+++ src/Item.js
@@ -1,9 +1,12 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography, useMediaQuery } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { useTheme } from "@mui/material/styles";
 
 export default function Item({ item, onDelete, onUpdate }) {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
   const handleIncrement = () => {
     const newCount = item.count + 1;
     onUpdate(item.id, newCount);
@@ -16,7 +19,7 @@ export default function Item({ item, onDelete, onUpdate }) {
     }
   };
 
-  return (
+  return !isSmallScreen ? (
     <Box
       sx={{
         width: "80%",
@@ -58,6 +61,46 @@ export default function Item({ item, onDelete, onUpdate }) {
       >
         Remove
       </Button>
+    </Box>
+  ) : (
+    <Box
+      sx={{
+        width: "95%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "1px 0",
+        padding: 1,
+        backgroundColor: "white",
+        borderRadius: 1,
+        boxShadow: 1,
+      }}
+    >
+      <Typography
+        sx={{
+          width: "50%",
+          textAlign: "center",
+          backgroundColor: "#808080",
+          borderRadius: 1,
+          padding: "2px",
+          boxShadow: 1,
+        }}
+      >
+        {item.name}
+      </Typography>
+      <Button
+        startIcon={<ArrowDropDownIcon />}
+        onClick={handleDecrement}
+      ></Button>
+      <Typography>{item.count}</Typography>
+      <Button
+        startIcon={<ArrowDropUpIcon />}
+        onClick={handleIncrement}
+      ></Button>
+      <Button
+        startIcon={<DeleteIcon />}
+        onClick={() => onDelete(item.id)}
+      ></Button>
     </Box>
   );
 }
